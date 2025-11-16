dbe1c5d1da15e49e536fd5407ab397c1fcc6e0ac
AbiramiAS <abiramishanmugamvkl@gmail.com>
2025-11-16 21:16:47 +0530

    Updated user login details

--- model/users.js
+++ model/users.js
@@ -1,20 +1,30 @@
-const userData = [
+export const userData = [
   {
-    username: "user1",
-    password: "ENB6JyoCvPjnuXpITtUd8x8",
+    "username": "user1",
+    "password": "ENB6JyoCvPjnuXpITtUd8x8"
   },
   {
-    username: "user2",
-    password: "L3eoQGYTm1z9lGy5iRf8D7NA",
+    "username": "user2",
+    "password": "L3eoQGYTm1z9lGy5iRf8D7NA"
   },
   {
-    username: "user3",
-    password: "QO2vziXAQRw/Nz7Hkkhaq3pv",
+    "username": "user3",
+    "password": "QO2vziXAQRw/Nz7Hkkhaq3pv"
   },
   {
-    username: "user4",
-    password: "usQUm5LoNJe8Fz5ps3aj8nZE",
+    "username": "user4",
+    "password": "usQUm5LoNJe8Fz5ps3aj8nZE"
   },
+  {
+    "username": "user5",
+    "password": "$2b$10$cLG0f3yFVLiblG58q5CDYu5HsdLHRGbuSfWw4gxYOcKScDQ90wFZa"
+  },
+  {
+    "username": "user7",
+    "password": "$2b$10$cAmcDRahyW0cNgJmgTDfq.NNSIXxBuJ.qyG65TybtEsLD9f86oQni"
+  },
+  {
+    "username": "user8",
+    "password": "$2b$10$EPUDGx7nTq/CplqJHxoVgeCJ.yRArUjNseJb79/aEOxHc14/gZ51W"
+  }
 ];
-
-export default userData;
